036aecf2073de6180916f925033122dcb86f2714
Heedong Kim <ruthetum@gmail.com>
2020-10-08 04:00:21 +0900

    [feat] 클라우드 DB 적용(mongo db atlas)

--- dbConnect.js
+++ dbConnect.js
@@ -1,12 +1,19 @@
+const dotenv = require("dotenv");
+dotenv.config();
 const mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 mongoose.set("useNewUrlParser", true);
 mongoose.set("useFindAndModify", false);
 mongoose.set("useCreateIndex", true);
 mongoose.set("useUnifiedTopology", true);
-const url = "mongodb://localhost:27017/mafia";
+const url = process.env.MONGO_DB_ATLAS;
 const connect = mongoose.connect(
-    url, { useNewUrlParser: true, useUnifiedTopology: true },
+    url,
+    { 
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    },
     () => {
         console.log("mongodb connected");
     }
